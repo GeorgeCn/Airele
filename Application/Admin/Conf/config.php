93095a1e98c037ef27f1e0d1e5b004778ffb3812
GeorgeCn <george18151162206@gmail.com>
2018-03-29 10:52:24 +0800

    3.29 重构tp3.2 airele后台管理系统

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -1,6 +1,7 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	// 默认过滤函数
 	'DEFAULT_FILTER'        => 'strip_tags,htmlspecialchars',
 
 	// 开启路由
@@ -17,9 +18,13 @@ return array(
 	'__UPLOAD__' => '__ROOT__/Public/Uploads/', // 增加新的上传路径替换规则
 	),
 
-	//'TMPL_FILE_DEPR'=>'_',
-     'RONGLIAN_ACCOUNT_SID'   => '8a216da85805311d015818586c2b0860', //容联云通讯 主账号 accountSid
+	//'RONGLIAN_CONFIG'
+    'RONGLIAN_ACCOUNT_SID'   => '8a216da85805311d015818586c2b0860', //容联云通讯 主账号 accountSid
     'RONGLIAN_ACCOUNT_TOKEN' => '994895187b114f44b6692103fb1332da', //容联云通讯 主账号token accountToken
     'RONGLIAN_APPID'         => '8a216da85805311d015818586ca50865', //容联云通讯 
 
+    //'MYSQL_RULE'
+    'BACKUP_URL' => './Data',           //数据库备份目录
+    'OPERATING_PASSWORD' => '923143925', //操作密码
+    'SYSTEM_MYSQL_TABLE' => array('admin','auth_cate','char'),//系统保留表,禁止删除,否则报错
 );
